8a15237b48cebdd8bbf5ea4e8cc56c0cc699ef4c
Travis212121 <99341931+Travis212121@users.noreply.github.com>
2022-05-09 19:24:38 -0400

    Update Character.java

--- Sprint 1 (V1.1)/Character.java
+++ Sprint 1 (V1.1)/Character.java
@@ -1,11 +1,8 @@
 /**
-* Names:  Larry Li
-          Shairahavan Selvachandran
-          Kevin Yao
-          Cyrus Yang
+* Names:  Shairahavan Selvachandran
 * Class: ICS4U1-5A
 * Date: April 29th, 2022
-* Description: Basic Character to be controlled (very bare bones).
+* Description: Basic Character to be controlled.
 */
 
 public class Character {
