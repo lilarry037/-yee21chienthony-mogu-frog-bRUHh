f6d5776a41e8b131d3fff550a80799cc272d4871
Travis212121 <99341931+Travis212121@users.noreply.github.com>
2022-05-09 19:27:48 -0400

    Update Inventory.java

--- Sprint 1 (V1.1)/Inventory.java
+++ Sprint 1 (V1.1)/Inventory.java
@@ -1,8 +1,5 @@
 /**
 * Names:  Larry Li
-          Shairahavan Selvachandran
-          Kevin Yao
-          Cyrus Yang
 * Class: ICS4U1-5A
 * Date: April 29th, 2022
 * Description: write this later
