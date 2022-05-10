37edd954ce61988a982d19ae95777b1a62b3e028
A Programmer <99341645+contingentUser45@users.noreply.github.com>
2022-05-10 12:46:00 -0400

    Update Hostile_Mob.java

--- Sprint 1 (V1.1)/Hostile_Mob.java
+++ Sprint 1 (V1.1)/Hostile_Mob.java
@@ -2,7 +2,26 @@
 * Names:  Cyrus Yang
 * Class: ICS4U1-5A
 * Date: April 29th, 2022
-* Description: Basic Character to be controlled.
+* Description: mob hostile
 */
 
+public class Hostile_Mob extends Mob {
+
+   public Item inHand;
+   public int damage;
+
+    public Hostile_Mob (String name, int damage, int x, int y, Item inHand){
+      super(name, x, y);
+      this.damage = damage;
+      this.inHand = inHand;
+   }
+
+    public void setDamage(int newDamage) {
+      this.damage = newDamage;
+   }
+
+   public void attack(Character player) {
+
+   }
+}
 
