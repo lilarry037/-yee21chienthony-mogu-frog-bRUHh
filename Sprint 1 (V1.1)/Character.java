615d1e29e1c8ef1b6f17172e7dcea087c6ac9e7f
Travis212121 <99341931+Travis212121@users.noreply.github.com>
2022-05-10 01:05:59 -0400

    Update Character.java

--- Sprint 1 (V1.1)/Character.java
+++ Sprint 1 (V1.1)/Character.java
@@ -7,16 +7,19 @@
 import java.util.*;
 public class Character {
    
+   // attributes
    private String name;
    private int health;
    private int hunger;
-   private int xp;
+   public int xp;
    private int x;
    private int y;
-
+   
+   // constants
    private static int MAX_HEALTH= 20;
    private static int MAX_HUNGER = 20;
-
+   
+   // Constructor
    public Character (String name, int xp, int x, int y){
       this.name = name;
       this.health = MAX_HEALTH;
@@ -83,43 +86,57 @@ public class Character {
    }
     
    //Methods
-
+   
+   // the character takes damage
    public void takeDamage(int amount){
       health -= amount;
+      System.out.println("Owwwww. Health Bar: " + this.health);
       if(health <= 0){
-         //Death Messages
+         System.out.println("One too many punches.");
          System.exit(0);
       }
    }
-
+   
+   // the character heals (boosts health bar)
    public void heal(int amount){
-      health =+ amount;
+      health += amount;
       if(health > MAX_HEALTH){
          health = MAX_HEALTH;
+         System.out.println("Already at full health.");
+      } else {
+         System.out.println("Healing! Health Bar: " + this.health);
       }
    }
-
+   
+   // the character becomes hungry (hunger bar decreases)
    public void becomeHungry(int amount){
       hunger -= amount;
       if (hunger <= 0){
          hunger = 0;
-         //Death messages
+         System.out.println("Food...Water...Atmosphere...blah");
          System.exit(0);
-      }
-   }
-
-   public void eat(Food foodEaten){
-      hunger =+ foodEaten.hungerBoost;
-      if(hunger > MAX_HUNGER){
-         health = MAX_HUNGER;
-      }
-      
-      hunger =+ foodEaten.healthBoost;
-      if(hunger > MAX_HEALTH){
-         health = MAX_HEALTH;
+      } else {
+         System.out.println("Losing energy. Hunger Bar: " + this.hunger);
       }
    }
    
+   // eat a food (effects depend on attributes)
+   public void eat(Food foodEaten){
+      hunger += foodEaten.hungerBoost;
+      if(hunger > MAX_HUNGER){
+         health = MAX_HUNGER;
+         System.out.println("Already full.");
+      } else {
+         System.out.println("Yum! This " + foodEaten.name + " tastes great! Hunger Bar: " + this.hunger);
+      }
+      
+      hunger += foodEaten.healthBoost;
+      if(hunger > MAX_HEALTH){
+         health = MAX_HEALTH;
+      } 
+   }
+   
+   // character moves from old spot to current dimensions
    public void move(int newX, int newY) {
       this.setX(newX);
       this.setY(newY);
@@ -127,6 +144,7 @@ public class Character {
       System.out.println("The Character moved to the new coordinates " + newX + ", " + newY + "!");
    }
    
+   // player attacks a non-playing character (mob)
    public void attack(Weapon weaponUsed, Mob npc) {
        
       double distance;
@@ -135,34 +153,19 @@ public class Character {
       
       if (distance <= weaponUsed.damageRadius){
          npc.health -= weaponUsed.damage;
+         System.out.println("Clang. You have hit the " + npc.name + " for " + weaponUsed.damage + " using the " + weaponUsed.name + ".");
          if (npc.health <= 0) {
             //mob dies
-            System.out.println("You have killed" + npc.name + "!");
+            System.out.println("You have killed the " + npc.name + "!");
+            this.xp += 10;
+            System.out.println("You have gained 10 XP. You now have " + this.xp + " XP.");
          }
       } else {
-         System.out.println("Out of range.");
+         System.out.println("Out of range for " + weaponUsed.name + ".");
       }
    }
-   
-   public void farm(Weapon tool, Block block) {
-       
-      double distance;
-      
-      // replace with block.x and block.y
-      distance = Math.sqrt(Math.pow((this.x-4),2) + Math.pow((this.y-4),2));
-       
-      if (tool.farmingTool == true){
-         if (distance <= tool.damageRadius) {
-            // increase inventory
-            // check if it exceeds maxStack (return to maxStack if so)
-         } else {
-            System.out.println("Out of range.");
-         }
-      } else {
-         System.out.println("Inadequate farming tool.");
-      }  
-   }
 
+   // player mines block
    public void mineBlock(int x, int y, int pickPower) {
       //Generating instances of the blocks
       Block[] newBlock;
@@ -171,19 +174,20 @@ public class Character {
       int randomNumber = (int)(Math.random() * ((6 - 0) + 1)) + 0;  
       newBlock[randomNumber].breakBlock(x, y, pickPower, newBlock[randomNumber].getDurability()); 
    }
-
+   
+   // toString method
    public String toString(){
       String output = "";
-      output += "Name: ";
+      output += "\nName: ";
       output += name;
-      output += "Health: ";
+      output += "\nHealth: ";
       output += health;
       output += "\nHunger: ";
       output += hunger;
       output += "\nXP: ";
       output += xp;
-      output += "Coordinates: (";
-      output += x + ", " + y;
+      output += "\nCoordinates: (";
+      output += x + ", " + y + ")";
       
       return output;
    }
