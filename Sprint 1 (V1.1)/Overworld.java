cec9ee22bce2a694ac2e551d565199fdc0d2d70e
Kevin Yao <kevin.yao@student.tdsb.on.ca>
2022-05-09 22:58:50 -0400

    help

--- Sprint 1 (V1.1)/Overworld.java
+++ Sprint 1 (V1.1)/Overworld.java
@@ -1,41 +1,26 @@
 public class Overworld {
-
-public WorldBuilder(){ //Allows you to make an instance of the class
-    }
-    
       
-    public static String createWorld(Character c1) {
+    public static String createWorld(Character player) {
         //Creating instances of blocks that can be used to generate terrain
-        Block[] blocks;
-        Block[] createdBlocks = new Block[4]; 
+        Block[] listOfBlocks;
+        Block[] createdBlocks = new Block[7]; 
         createdBlocks = Block.listOfBlocks();
         
         //Making a 1000 by 1000 grid for the world
-        PlacedBlock[][] generatedBlocks = new PlacedBlock[1000][1000];
+        Block[][] world = new Block[1000][1000];
         //Random number generator 
-        Random randomNumber = new Random();
-        int answer;
+        int randomNumber = (int)(Math.random() * ((6 - 0) + 1)) + 0;
         
         //For loop to get a random number and get a block based on that number
         for (int i = 0; i < 1000; i++) {
             for (int j = 0; j < 1000; j++) {
-                answer = rn.nextInt(4);
                 //Adding the random block to the grid
-                generatedBlocks[i][j] = createdBlocks[answer]; 
+                world[i][j] = createdBlocks[randomNumber]; 
             }  
         }
         
-        String world = "";
-        
         //Printing the users coordinates
-        world += "Your coordinates: " + c1.getX() + " " + c1.getY() + "(You are on)\n";
-        
-        //for loop to make the actual output from the type of blocks that were randomly generated
-        for (int i = 0; i < 3; i++) { //Could make a second for loop to make it make a larger terrain
-            //world += generatedBlocks[i][0].getType() + "|" + generatedBlocks[i][1].getType() + "|" + generatedBlocks[i][2].getType() + "\n";
-            //world += "----------------\n";
-        } 
-
-        return world;
+        String locationInWorld = "Your coordinates: " + player.getX() + " " + player.getY() + "(You are on a " + world[player.getX()][player.getY()].getName() + " block.";
+        return locationInWorld;
     }  
 }
