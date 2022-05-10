e580a3df4bd334ba6b4a50dce283812e84120c8d
A Programmer <99341645+contingentUser45@users.noreply.github.com>
2022-05-10 12:54:33 -0400

    Update Main.java

--- Sprint 1 (V1.1)/Main.java
+++ Sprint 1 (V1.1)/Main.java
@@ -101,9 +101,10 @@ class Main {
       
       //Killing the character
       //Death messages + conditions
-      
       player1.takeDamage(20);  // test damage taken
-      
+      Boolean killedbymob = true;
+      Condition(name, false, killedbymob, false)
+     
       System.exit(0);
    }
 }
