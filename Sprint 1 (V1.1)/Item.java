54fee0fc9003057f0a963570cd65f7b4890eddf2
lilarry037 <99341500+lilarry037@users.noreply.github.com>
2022-05-11 09:25:47 -0400

    Update Item.java

--- Sprint 1 (V1.1)/Item.java
+++ Sprint 1 (V1.1)/Item.java
@@ -9,103 +9,103 @@
 */
 
 public class Item {
-  
-   /*
-      Attributes
-   */
-   
-   /** the name of the item */
-   private String name;
-   
-   /** the ID of the item */
-   private int itemID;
-   
-   /** the maximum number of items in a stack */
-   private int maxStack;
-   
-   /*
-      Constructor
-   */
-   
-   /**
-   * Item
-   * Creates an item in minecraft.
-   * @param name: the name of the item
-   * @param itemID: the ID of the item
-   * @param maxStack: the maximum number in a stack for the item 
-   */
-   public Item (String name, int itemID, int maxStack) {
-      this.name = name;
-      this.itemID = itemID;
-      this.maxStack = maxStack;
-   }
-   
-   /*
-   Methods
-   */
-   
-   //Accessors
-   
-   /**
-   * Gets the name of item
-   * @return the item's name
-   */ 
-   public String getName() {
-      return this.name;
-   } 
-       
-   /**
-   * Gets the ID of item
-   * @return the item's ID
-   */
-   public int getItemID() {
-      return this.itemID;
-   }
-   
-   /**
-   * Gets the maximum stack allowable for the item
-   * @return the item's mazimum stack
-   */
-   public int getMaxStack() {
-      return this.maxStack;
-   }
-   
-   // Mutators
-   
-   /**
-   * changes name of iitem
-   * @param newName - new name of the item
-   */
-   public void setName(String newName) {
-      this.name = newName;
-   }
-   
-   /**
-   * changes ID of the item
-   * @param newID - new ID of item
-   */
-   public void setItemID(int newItemID) {
-      this.itemID = newItemID;
-   }
-   
-   /**
-   * changes the maximum amount allowable for a stack of the item
-   * @param newMaxStack - new max stack of item
-   */
-   public void setMaxStack(int newMaxStack) {
-      this.maxStack = newMaxStack;
-   }
 
-   // Methods
-   
-   /**
-   * toString
-   * Returns a string with the attributes of the item.
-   * @return returnString: 
-   */
-   public String toString() {
-      String returnString = "Name: " + this.name + "\nItem number: #" + this.itemID + "\nMax Stack: " + this.maxStack;
-      
-      return returnString;
-   }
+    /*
+       Attributes
+    */
+
+    /** the name of the item */
+    private String name;
+
+    /** the ID of the item */
+    private int itemID;
+
+    /** the maximum number of items in a stack */
+    private int maxStack;
+
+    /*
+       Constructor
+    */
+
+    /**
+     * Item
+     * Creates an item in minecraft.
+     * @param name: the name of the item
+     * @param itemID: the ID of the item
+     * @param maxStack: the maximum number in a stack for the item 
+     */
+    public Item(String name, int itemID, int maxStack) {
+        this.name = name;
+        this.itemID = itemID;
+        this.maxStack = maxStack;
+    }
+
+    /*
+    Methods
+    */
+
+    //Accessors
+
+    /**
+     * Gets the name of item
+     * @return the item's name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Gets the ID of item
+     * @return the item's ID
+     */
+    public int getItemID() {
+        return this.itemID;
+    }
+
+    /**
+     * Gets the maximum stack allowable for the item
+     * @return the item's mazimum stack
+     */
+    public int getMaxStack() {
+        return this.maxStack;
+    }
+
+    // Mutators
+
+    /**
+     * changes name of an item
+     * @param newName - new name of the item
+     */
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    /**
+     * changes ID of the item
+     * @param newID - new ID of item
+     */
+    public void setItemID(int newItemID) {
+        this.itemID = newItemID;
+    }
+
+    /**
+     * changes the maximum amount allowable for a stack of the item
+     * @param newMaxStack - new max stack of item
+     */
+    public void setMaxStack(int newMaxStack) {
+        this.maxStack = newMaxStack;
+    }
+
+    // Methods
+
+    /**
+     * toString
+     * Returns a string with the attributes of the item.
+     * @return returnString: 
+     */
+    public String toString() {
+        String returnString = "Name: " + this.name + "\nItem number: #" + this.itemID + "\nMax Stack: " + this.maxStack;
+
+        return returnString;
+    }
 }
