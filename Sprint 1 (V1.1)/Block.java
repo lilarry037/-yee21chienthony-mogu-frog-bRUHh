7765a42af4348802bebef634b1aa7ff1bed5849d
KevinYao01 <99771394+KevinYao01@users.noreply.github.com>
2022-05-10 20:46:35 -0400

    Update Block.java

--- Sprint 1 (V1.1)/Block.java
+++ Sprint 1 (V1.1)/Block.java
@@ -2,15 +2,33 @@
 * Names: Kevin Yao
 * Class: ICS4U1-5A
 * Date: April 29th, 2022
-* Description: Basic Character to be controlled.
+* Description: Block generator class
 */
 
 public class Block extends Item{
-   
+   /*
+    Attributes
+   */
+  
+   /** the x coordinate of the block */
    private int x;
+	
+   /** the y coordinate of the block */	
    private int y;
+	
+   /** the durability of the block */	
    private int durability;
-   
+	
+        /**
+        * Block
+   	* Description: Creates a block at a location with durability, ID, and maxStack
+    	* @param name: subclass under item, so takes in a name
+	* @param itemID: subclass under item, so takes in an ID
+	* @param maxStack: subclass under item, so has a maxStack
+	* @param x: x coordinate of where the block is going to be spawned
+	* @param y: y coordinate of where the block is going to be spawned
+	* @param durability: the "hp" of a block to work with a pickaxe to determine how many hits a block will take to break
+    	*/
 	public Block(String name, int itemID, int maxStack, int x, int y, int durability) {
 	  
 		super(name, itemID, maxStack);
@@ -23,15 +41,27 @@ public class Block extends Item{
    /*
       Accessors
    */
-
+	
+   /**
+   Gets the x coordinate of the block
+   @return the x coordinate of the block
+   */
    public int getX() {
       return this.x;
    }
    
+   /**
+   Gets the y coordinate of the block
+   @return the y coordinate of the block
+   */
    public int getY() {
       return this.y;
    }
    
+   /**
+   Gets the durability of the block
+   @return the durability of the block
+   */
    public int getDurability() {
       return this.durability;
    }
@@ -40,11 +70,20 @@ public class Block extends Item{
       Mutators
    */
    
+   /**
+   Sets the new location of the block
+   @param newX: new x coordinate of the block
+   @param newY: new y coordinate of the block
+   */
    public void setPlace(int newX, int newY) {
       this.x = newX;
       this.y = newY;
    }
-   
+	
+   /**
+   Changes the block durability to a new durability value
+   @param newDurability: new durability of the block
+   */
    public void setDurability(int newDurability) {
       this.durability = newDurability;
       if (this.durability < 0) { //If the blocks breaks
@@ -52,8 +91,10 @@ public class Block extends Item{
       }
    }
    
-  
-   //Basic list of blocks for the overworld
+   /**
+   Creates a list of standard block objects to use for generation in the Overworld
+   @return the array of blocks
+   */
    public static Block[] listOfBlocks() {
       //List of blocks that is returned
       Block[] listOfBlocks = new Block[7]; 
@@ -68,7 +109,13 @@ public class Block extends Item{
       return listOfBlocks;
    }
    
-   //Breaking a block
+   /**
+   Breaks a block
+   @param x: x coordinate of block to break
+   @param y: y coordinate of block to break
+   @param pickPower: power of pickaxe to break the block
+   @param durability: durability of block (this shouldnt have been necessary but I couldn't find a way to reference a block's attributes from in the overworld grid) 
+   */
    public void breakBlock(int x, int y, int pickPower, int durability) {
          
        int numberOfHitsToBreak = durability/pickPower;
@@ -81,6 +128,7 @@ public class Block extends Item{
        for(int i = 0; i < numberOfHitsToBreak; i++) {
          System.out.println("Clang");
        }
+	   
        //Outputting which block was mined and how long it took
        System.out.println("The block at x:" + x + " and y:" + y + " was mined!");
        System.out.println("It took " + numberOfHitsToBreak + " hit(s) to mine the block!");
